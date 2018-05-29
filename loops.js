63cdd1e8ff5b5bdfc901ba043f886ed7448d129b
Yohannes Abera <yohannesns@gmail.com>
2018-05-29 01:13:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,10 +2,10 @@ function forLoop(array){
   var myArray = [];
   for(var i =0; i <25; i++){
     if (i=== 1){
-    myArray.push('I am ${1} strange loops');
+    myArray.push(`I am ${1} strange loops`);
     }
     else{
-      myArray.push('I am ${i} strange loops');
+      myArray.push(`I am ${i} strange loops`);
     }
   }
   return myArray;
