68b328becde476b2dc0e644e52d3b28311aea10e
Yohannes Abera <yohannesns@gmail.com>
2018-05-29 00:20:21 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,12 @@
+function forLoop(array){
+  var myArray = [];
+  for(var i =0; i <25; i++){
+    if (array[i] === 1){
+    myArray.push("I am ${1} strange loops");
+    }
+    else{
+      myArray.push("I am ${i} strange loops");
+    }
+  }
+  return myArray;
+}
